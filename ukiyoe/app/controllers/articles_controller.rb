8121245eaf37fd1a29cba6b6c7ab5df2e3f375db
Roy Xue <xljroy@gmail.com>
2015-01-14 00:00:40 +0800

    update article controller

--- ukiyoe/app/controllers/articles_controller.rb
+++ ukiyoe/app/controllers/articles_controller.rb
@@ -10,6 +10,7 @@ class ArticlesController < ApplicationController
   # GET /articles/1
   # GET /articles/1.json
   def show
+    @article = Article.find(params[:id])
   end
 
   # GET /articles/new
@@ -19,6 +20,7 @@ class ArticlesController < ApplicationController
 
   # GET /articles/1/edit
   def edit
+    @article = Article.find(params[:id])
   end
 
   # POST /articles
@@ -69,6 +71,6 @@ class ArticlesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-      params.require(:article).permit(:title, :content, :create_at, :last_edit, :status, :like)
+      params.require(:article).permit(:title, :content, :status)
     end
 end
