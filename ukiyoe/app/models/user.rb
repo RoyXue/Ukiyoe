91dda0704159161f6871296caa5af0139cb33302
Lijun Xue <xljroy@gmail.com>
2015-02-27 23:10:11 +0800

    update model validation

--- ukiyoe/app/models/user.rb
+++ ukiyoe/app/models/user.rb
@@ -5,12 +5,14 @@ class User < ActiveRecord::Base
 
   validates :pwd, presence: true, confirmation: true, length: {minimum: 8, too_short: "password should be more than %{count} characters"}
   validates :pwd_confirmation, presence: true
-
-  validates :email, confirmation: true, format: { with: /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/, message: "Invalid email address!" }
-  validates :email_comfirmation, presence: true
+  
+  EMAIL_REGEX = /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/i
+  validates :email, presence: true, format: { :with=>EMAIL_REGEX, :message => "Invalid email address!" }
 
   def default
     self.profile ||="This User is too lazy to leave a word"
+    self.created_at ||=Time.current
+    self.updated_at ||=Time.current
   end
 
 end
