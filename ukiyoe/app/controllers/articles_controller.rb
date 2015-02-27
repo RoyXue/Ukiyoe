24dfba259740e749cae182ed49131646223ae2d4
Lijun Xue <xljroy@gmail.com>
2015-02-27 23:10:46 +0800

    update form

--- ukiyoe/app/controllers/articles_controller.rb
+++ ukiyoe/app/controllers/articles_controller.rb
@@ -71,6 +71,6 @@ class ArticlesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-      params.require(:article).permit(:title, :content, :status, :user_id, :tag_id, :category_id)
+      params.require(:article).permit(:title, :content, :status, :user_id, :category_id)
     end
 end
